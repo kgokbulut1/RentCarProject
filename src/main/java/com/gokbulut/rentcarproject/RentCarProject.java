12685c845a0f88c773df9dd9448039fec05d5db5
kadirgokbulut1 <kadirgokbulut1@gmail.com>
2025-11-16 14:01:49 +0300

    Rent a Car - Part 1 - Java Backend Projesi - Docker - PostgreSQL - CRUD İşlemleri - 1

---
+++ src/main/java/com/gokbulut/rentcarproject/RentCarProject.java
@@ -0,0 +1,12 @@
+package com.gokbulut.rentcarproject;
+
+/**
+ *
+ * @author Atlas
+ */
+public class RentCarProject {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rent Car Project");
+    }
+}
